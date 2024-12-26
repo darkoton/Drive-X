c64055cd80b7c11761071312b857591ec1e654c9
Your Name <you@example.com>
2024-12-27 00:45:41 +0200

    add block order, reviews

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,37 +1,62 @@
-const mainSlider = new Swiper('.main__banners-slider', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  grabCursor: true,
-  loop: true,
-  speed: 800,
+const main = document.querySelector('.main__banners-slider')
+let mainSlider
+if (main) {
+  mainSlider = new Swiper(main, {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    speed: 800,
 
-  pagination: { el: '.main__banners-pagination' },
-});
+    pagination: { el: '.main__banners-pagination' },
+  });
+}
 
-const productsSlider = new Swiper('.products__slider', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  grabCursor: true,
 
-  pagination: { el: '.products__pagination' },
+const products = document.querySelector('.products__slider')
+let productsSlider
+if (products) {
+  productsSlider = new Swiper(products, {
+   slidesPerView: 4,
+   spaceBetween: 30,
+   grabCursor: true,
 
-  navigation: {
-    nextEl: '.products__next',
-    prevEl: '.products__prev',
-  },
+   pagination: { el: '.products__pagination' },
 
-  breakpoints:{
-    0:{
-      slidesPerView: 2,
-      spaceBetween: 10,
+   navigation: {
+     nextEl: '.products__next',
+     prevEl: '.products__prev',
+   },
+
+   breakpoints:{
+     0:{
+       slidesPerView: 2,
+       spaceBetween: 10,
+     },
+     680:{
+       slidesPerView: 3,
+       spaceBetween: 10,
+     },
+     1300:{
+       slidesPerView: 4,
+       spaceBetween: 30,
+     }
+   }
+ });
+}
+
+const reviews = document.querySelector('.reviews__slider')
+let reviewsSlider
+if (reviews) {
+  reviewsSlider = new Swiper(reviews, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    grabCursor: true,
+    speed: 800,
+
+    navigation: {
+      nextEl: '.reviews__slider-next',
+      prevEl: '.reviews__slider-prev',
     },
-    680:{
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    1300:{
-      slidesPerView: 4,
-      spaceBetween: 30,
-    }
-  }
-});
+  });
+}
