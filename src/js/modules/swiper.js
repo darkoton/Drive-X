fbc37e3f39140373eabecd896adebabe5f1ecd2b
Your Name <you@example.com>
2024-12-26 19:12:48 +0200

    add products block

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -12,17 +12,26 @@ const productsSlider = new Swiper('.products__slider', {
   slidesPerView: 4,
   spaceBetween: 30,
   grabCursor: true,
-  loop: true,
-  speed: 800,
-
-  autoplay: {
-    delay: 3500,
-  },
 
   pagination: { el: '.products__pagination' },
 
   navigation: {
-    nextEl: '.products__prev',
-    prevEl: '.products__next',
+    nextEl: '.products__next',
+    prevEl: '.products__prev',
   },
+
+  breakpoints:{
+    0:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    680:{
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1300:{
+      slidesPerView: 4,
+      spaceBetween: 30,
+    }
+  }
 });
