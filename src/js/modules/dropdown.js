90e5a4a6e0a11f6454f2ac6a271c9f55d93724f7
Your Name <you@example.com>
2024-12-28 01:41:19 +0200

    start create categories page

--- src/js/modules/dropdown.js
+++ src/js/modules/dropdown.js
@@ -2,15 +2,22 @@ const dropdowns = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropdown => {
   const button = dropdown.querySelector('.dropdown-open');
+  const value = dropdown.querySelector('.dropdown-open > span');
   const list = dropdown.querySelector('.dropdown-list');
 
   button.onclick = () => {
-    dropdown.classList.add('active');
+    dropdown.classList.toggle('active');
   };
 
   list.querySelectorAll('li').forEach(li => {
     li.onclick = () => {
       dropdown.classList.remove('active');
+
+      if(dropdown.classList.contains('no-value')){
+        return;
+      }
+      value.textContent = li.textContent
+      value.dataset.value = li.dataset.value
     };
   });
 });
